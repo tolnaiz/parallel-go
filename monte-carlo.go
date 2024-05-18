00a9567b9da3ec1c46b6732e1906627daae33947
Zoltan Tolnai <tolnaiz@gmail.com>
2024-05-18 17:00:15 +0200

    Add error calculation and iteration parameter

--- monte-carlo.go
+++ monte-carlo.go
@@ -1,10 +1,21 @@
 package main
 
 import "fmt"
+import "math"
 import "math/rand"
+import "os"
+import "strconv"
 
 func main() {
 	var npoints = 1000000
+	var err error
+	if len(os.Args) > 1 {
+		npoints, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	var circle_count = 0
 	var pi = 0.0
 	for i := 0; i < npoints; i++ {
@@ -13,8 +24,8 @@ func main() {
 		if (x*x + y*y) <= 1 {
 			circle_count++
 		}
-		pi = 4.0 * float64(circle_count) / float64(npoints)
-		fmt.Printf("%f at iteration %d \r", pi, i)
+		pi = 4.0 * float64(circle_count) / float64(i)
+		fmt.Printf("%.10f (error %.10f) at iteration %d \r", pi, math.Pi - pi, i)
 	}
 	fmt.Print("\n")
 }
